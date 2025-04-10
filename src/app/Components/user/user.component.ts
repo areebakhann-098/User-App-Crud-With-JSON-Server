a15bf433992be72a2150e98c38f25dc38b1ebcb5
areeba-wq <areebakhalid9854@gmail.com>
2025-04-10 12:47:37 +0500

    update

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../Model/user';
 import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { UserService } from '../../Services/user.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule], 
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrls: ['./user.component.css'] 
 })
 export class UserComponent implements OnInit {
   userList: UserModel[] = [];
@@ -26,42 +27,37 @@ export class UserComponent implements OnInit {
     address: "",
     status: false,
   }
+
+  cityList: string[] = ["Abbottabad", "Multan", "Karachi", "Islamabad", "Faisalabad"];
+  departmentList: string[] = ["IT", "HR", "Accounts", "Sales", "Management"];
+
   constructor(private _userService: UserService, private _toastrService: ToastrService) { }
+
   ngOnInit(): void {
     this.getUserList();
   }
-  cityList: string[] = ["Abbottabad", "Multan", "Karachi", "Islamabad", "Faisalabad"];
-  departmentList: string[] = ["IT", "HR", "Accounts", "Sales", "Management"];
 
   getUserList() {
     this._userService.getUsers().subscribe((res) => {
       this.userList = res;
-
     });
   }
+
   onSubmit(form: NgForm): void {
-    debugger;
     if (this.editMode) {
-      console.log(form);
       this._userService.updateUser(this.user).subscribe((res) => {
         this.getUserList();
         this.editMode = false;
         form.reset();
         this._toastrService.success('User Updated Successfully', 'Success');
-
       });
-    }
-    else {
-      console.log(form);
+    } else {
       this._userService.addUser(this.user).subscribe((res) => {
         this.getUserList();
         form.reset();
         this._toastrService.success('User Added Successfully', 'Success');
-
       });
     }
-
-
   }
 
   onEdit(userdata: UserModel) {
@@ -77,16 +73,16 @@ export class UserComponent implements OnInit {
         this.getUserList();
       });
     }
-
   }
+
   onResetForm(form: NgForm) {
     form.reset();
     this.editMode = false;
     this.getUserList();
   }
+
   searchUserByName($event: Event) {
     const input = ($event.target as HTMLInputElement).value.toLowerCase();
-  
     if (input) {
       this.userList = this.userList.filter(user =>
         user.name.toLowerCase().includes(input)
@@ -95,6 +91,4 @@ export class UserComponent implements OnInit {
       this.getUserList(); 
     }
   }
-  
-  
 }
